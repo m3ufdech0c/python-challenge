0067d14b2e38525ff43fd4b70c7095f4edeb19d8
m3ufdech0c <aidadaz@gmail.com>
2023-01-03 14:58:57 -0600

    Completed script for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from statistics import mean
 
 # Create a path to the PyBank csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
@@ -9,14 +10,58 @@ with open(csvpath) as csvpybank:
     csvreader = csv.reader(csvpybank, delimiter=',')
     csv_header = next(csvreader)
     
-    # Lists to store day and month from the upcoming split of the date column
+    # Lists to store data we will need to loop through
     day = []
     month = []
+    pnl = []
+    losses = []
+    change = []
+    # Initializing variable 'previous' to be used for computing changes withing the 'PnL' list
+    previous = 0 
+    isfirstrow = True
+    maxchange = 0
+    minchange = 0
 
     # Read each row of data after the header
     for row in csvreader:
-        # Split the date into day and month
+        # Split the date column into day and month data to append to their respective lists
         split_date = row[0].split("-")
         day.append(split_date[0])
         month.append(split_date[1])
-    print(f"Total Months: {len(month)}")    
+
+        # Add PnL data
+        pnl.append(int(row[1]))
+        
+        # Here we are adding data to the "Change" list starting from row#2, while computing the differences between subsequent elements of the 'PnL' list
+        if isfirstrow == False:
+            change.append(int(row[1]) - previous)
+            # Looping through the change list to find the maximum value by using the max() function and comparing it to the initial value of maxchange, notice 
+            if max(change) != maxchange:
+                maxchange = int(row[1]) - previous
+                # Assigning the date for that instance where the change is the greatest as the maxchange date
+                maxchangedate = row[0]
+            # Looping through the change list to find the min value by using the min() function and comparing it to the initial value of minchange, notice 
+            if min(change) != minchange:
+                minchange = int(row[1]) - previous
+                minchangedate = row[0]
+
+    
+
+        previous = int(row[1])
+
+        isfirstrow = False
+      
+    # This could have been used to create the 'Change' but did not want another loop
+    #for i in range(1, len(pnl)):
+        #value = pnl[i] - pnl[i - 1]
+        #change.append(value)
+    
+    print("Financial Analysis")
+
+    print("------------------------------------------------------")
+
+    print(f"Total Months: {len(month)}")
+    print(f"Total: ${sum(pnl)}")
+    print(f"Average Change: ${round(mean(change), 2)}")
+    print(f"Greatest Increase in Profits: {maxchangedate} (${maxchange})")   
+    print(f"Greatest Decrease in Profits: {minchangedate} (${minchange})") 
